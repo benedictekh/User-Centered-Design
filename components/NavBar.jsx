9787daa9f3eed82beb52cc0833503730ea148cdd
benedictekh <benedicte.kh@gmail.com>
2022-01-04 13:25:49 +0000

    Created navBar component

---
+++ components/NavBar.jsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import ProfileScreen from './ProfileScreen';
+import HomeScreen from './HomeScreen';
+
+const Tab = createBottomTabNavigator();
+
+function MyTabs() {
+  return (
+    <Tab.Navigator>
+      <Tab.Screen name="Home" component={HomeScreen} />
+      <Tab.Screen name="Profile" component={ProfileScreen} />
+    </Tab.Navigator>
+  );
+}
+
+export default function NavBar() {
+  return (
+    <NavigationContainer>
+      <MyTabs />
+    </NavigationContainer>
+  );
+}
