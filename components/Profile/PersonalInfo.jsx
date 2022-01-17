07ee637c17786b28109615659b63f9457107742f
Inger-Ane Schefte <inger-aness@hotmail.com>
2022-01-17 11:38:41 +0000

    Profile w/o design finished

--- components/Profile/PersonalInfo.jsx
+++ components/Profile/PersonalInfo.jsx
@@ -1,36 +1,51 @@
-import React, { useState } from "react";
-import { View, ScrollView, Text, StyleSheet } from "react-native";
-import { Avatar, Image, Card, Button } from "react-native-elements";
+import React from "react";
+import { View, Text, StyleSheet } from "react-native";
+import { Avatar, Card, Button, Badge } from "react-native-elements";
 
 const PersonalInfo = () => {
   return (
     <View>
-      <View>
-        <Card>
-          <Card.Title>Joana Alves</Card.Title>
-          <Avatar
-            title=""
-            size="xlarge"
-            rounded={true}
-            source={{ uri: "https://randomuser.me/api/portraits/" }}
-          ></Avatar>
+      <Card style={styles.profileCardStyle}>
+        <Card.Title>Joana Alves</Card.Title>
+        <Avatar
+          title=""
+          size="xlarge"
+          rounded={true}
+          source={{ uri: "https://randomuser.me/api/portraits/" }}
+        ></Avatar>
 
-          <Text>NIF: 99999</Text>
-          <Text>Address: Av. Rovisco Pais 1, 1049-001 Lisboa</Text>
-          <Text>Phone: +351 000 0000 000</Text>
-          <Text>email: joana@mail.pt</Text>
+        <Text>NIF: 99999</Text>
+        <Text>Address: Av. Rovisco Pais 1, 1049-001 Lisboa</Text>
+        <Text>Phone: +351 000 0000 000</Text>
+        <Text>email: joana@mail.pt</Text>
 
-          <Card.Divider />
-          <Text>Tokens</Text>
-          <Button title={"Transaction History"}></Button>
+        <Card.Divider />
+        <Text>Tokens</Text>
+        <Button
+          title={"Transaction History"}
+          style={styles.profileButtons}
+        ></Button>
 
-          <Card.Divider />
-          <Text>Want your personal DupmIt card?</Text>
-          <Button title={"Order Card"}></Button>
-        </Card>
-      </View>
+        <Card.Divider styles={styles.cardDivider} />
+        <Text>Want your personal DupmIt card?</Text>
+        <Button title={"Order Card"} style={styles.profileButtons}></Button>
+      </Card>
     </View>
   );
 };
+
+//styling
+const styles = StyleSheet.create({
+  profileCardStyle: {
+    backgroundColor: "red",
+  },
+  cardDivider: {
+    marginBottom: 10,
+    marginTop: 10,
+  },
+  profileButtons: {
+    color: "white",
+  },
+});
 
 export default PersonalInfo;
