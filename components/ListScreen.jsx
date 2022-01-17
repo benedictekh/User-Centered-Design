772aa2030b0c3ecc612cef30c03cf222d5fab1c8
benedictekh <benedicte.kh@gmail.com>
2022-01-17 12:45:08 +0000

    Added buttons for filter and map

--- components/ListScreen.jsx
+++ components/ListScreen.jsx
@@ -1,17 +1,37 @@
 import * as React from 'react';
 import { Text, View, ScrollView } from 'react-native';
+import { Button, Image } from 'react-native-elements';
 import {TrashcansList, ShopsList} from './List';
 import Searchbar from './Searchbar';
 
 export default function ListScreen() {
+    let image_path_filter = require('../assets/filter.png')
+    let image_path_map = require('../assets/map.png')
+
     return (
-        // <View>
         <ScrollView>
-        <View style={{ flex: 1, flexDirection: 'column',  alignItems: 'center' }}> 
-        <Searchbar />
-        <ShopsList />
-        </View>
-      </ScrollView>
+            <View style={{ flex: 1, flexDirection: 'row',  alignItems: 'center', justifyContent: 'space-around'}}>
+                <Image 
+                    source={image_path_filter} 
+                    style={{width:40, height:40}}
+                    onPress={() => console.log('pressed filter')}
+                ></Image>
+                <Button
+                    title={'Boolean switch'}
+                    buttonStyle={{backgroundColor: 'rgba(78, 116, 289, 1)',borderRadius: 3}}
+                    containerStyle={{width: 150,marginVertical: 10}}
+                ></Button>
+                <Image 
+                    source={image_path_map} 
+                    style={{width:40, height:40}}
+                    onPress={() => console.log('pressed map')}
+                ></Image>
+            </View>
+            <View style={{ flex: 1, flexDirection: 'column',  alignItems: 'center' }}> 
+                <Searchbar />
+                <ShopsList />
+            </View>
+        </ScrollView>
     );
   }
   
