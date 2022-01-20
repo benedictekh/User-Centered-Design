65c7709c6a888f43f881d9782c0ff82eaa16e9c8
Inger-Ane Schefte <inger-aness@hotmail.com>
2022-01-20 11:47:00 +0000

    wip

--- components/HelpPage/Questions.jsx
+++ components/HelpPage/Questions.jsx
@@ -6,34 +6,64 @@ const Questions = () => {
   return (
     <View>
       <Card>
-        <Text>Where can I spend my token?s</Text>
-        <Text>
+        <Text style={styles.questionStyle}>Where can I spend my token?</Text>
+
+        <Text style={styles.answerStyle}>
           In the list or map page, there's an option called 'shops' where you
           can see nearby adherent shops where you can spend your tokens.
         </Text>
-        <Card.Divider></Card.Divider>
-        <Text>How can I spend my tokens?</Text>
-        <Text>
+
+        <Card.Divider style={styles.dividerStyle}></Card.Divider>
+        <Text style={styles.questionStyle}>How can I spend my tokens?</Text>
+
+        <Text style={styles.answerStyle}>
           By clicking on the main page (the QR code) and scanning it at the
           store after registering your items at the cash register.
         </Text>
-        <Card.Divider></Card.Divider>
-        <Text>How can I get tokens when throwing out the garbage?</Text>
-        <Text>
+
+        <Card.Divider style={styles.dividerStyle}></Card.Divider>
+
+        <Text style={styles.questionStyle}>
+          How can I get tokens when throwing out the garbage?
+        </Text>
+
+        <Text style={styles.answerStyle}>
           Click on the main page (the QR code) and scan it on the trashcans'
           available scanners. Throw away the trash as usual, and you're done!
         </Text>
-        <Card.Divider></Card.Divider>
-        <Text>Is there a trashcan nearby?</Text>
-        <Text>
+
+        <Card.Divider style={styles.dividerStyle}></Card.Divider>
+        <Text style={styles.questionStyle}>Is there a trashcan nearby?</Text>
+
+        <Text style={styles.answerStyle}>
           You can search for this by opening the 'list' menu and changing to the
           map version of the available trashcans. Check your current location
           and where the nearest available trashcan is!
         </Text>
-        <Card.Divider></Card.Divider>
+
+        <Card.Divider style={styles.dividerStyle}></Card.Divider>
       </Card>
     </View>
   );
 };
+
+//styling
+const styles = StyleSheet.create({
+  questionStyle: {
+    fontWeight: "bold",
+    fontFamily: "Open Sans",
+    fontSize: 20,
+    backgroundColor: "red",
+  },
+  answerStyle: {
+    marginBottom: 10,
+    marginTop: 10,
+    color: "#3c3c43",
+  },
+  dividerStyle: {
+    marginBottom: 20,
+    marginTop: 20,
+  },
+});
 
 export default Questions;
